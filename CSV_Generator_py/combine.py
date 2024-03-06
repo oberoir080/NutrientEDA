bdbd21e448d0909349fa96588f1fc4fa1568fd8a
Saumil <saumil21097@iiitd.ac.in>
2024-03-06 14:25:54 +0530

    combine

---
+++ CSV_Generator_py/combine.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+
+with open("Data/FNDDS_2009_2010/FNDDSNutVal.txt", "r") as file:
+    lines1 = file.readlines()
+
+
+columns1 = ["Food Index", "Nutrient Index", "From", "To", "Amount"]
+
+# Split each line and create a list of lists for Food Index
+data1 = [line.strip().split('^')[:5] for line in lines1]
+
+
+
+food_df = pd.DataFrame(data1, columns=columns1)
+
+with open("Data/FNDDS_2009_2010/NutDesc.txt", "r") as file:
+    lines2 = file.readlines()
+
+
+columns2 = ["Nutrient Index", "Nutrient", "Nutrient Code", "Unit", "Decimal Places"]
+
+# Split each line and create a list of lists for Nutrient Index
+data2 = [line.strip().split('^')[:5] for line in lines2]
+
+# Create DataFrame for Nutrient Index
+nutrient_df = pd.DataFrame(data2, columns=columns2)
+
+nutrient_df["Nutrient"] = nutrient_df["Nutrient"].str.replace('~', '')
+nutrient_df["Nutrient Code"] = nutrient_df["Nutrient Code"].str.replace('~', '')
+nutrient_df["Unit"] = nutrient_df["Unit"].str.replace('~', '')
+
+
+combined_df = pd.merge(food_df, nutrient_df, on="Nutrient Index", how="inner")
+
+
+combined_df.to_csv("combined_output.csv", index=False)
+
+print("Combined CSV file has been saved as combined_output.csv")
+
+
+
